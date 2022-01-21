c5cd49f4a8cfeccf55267bf0430b491138aa55ef
bojan.churlinov <bchurlinov@hotmail.com>
2022-01-21 11:28:18 +0100

    added nodemon

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-import express = require('express');
-import path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 // This will create a middleware.
 // When you navigate to the root page, it would use the built react-app
-app.use(express.static(path.resolve(__dirname, '../build/')));
+app.use(express.static(path.resolve(__dirname, "../build/")));
