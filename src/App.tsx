eb6da280d208c3c1ee02187f0f1b97f7eaeaac0c
bojan.churlinov <bchurlinov@hotmail.com>
2022-01-21 09:56:26 +0100

    Changed to Production

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = (): JSX.Element => (
   <div className="App">
     <header className="App-header">
       <h1>
-        This is Development
+        This is Production
       </h1>
     </header>
   </div>
