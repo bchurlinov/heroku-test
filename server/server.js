59e71bdf0c5fe8c37c88876442fe9aa5b8ef8b48
bojan.churlinov <bchurlinov@hotmail.com>
2022-01-21 12:20:33 +0100

    custom IP middleware

--- server/server.js
+++ server/server.js
@@ -1,16 +1,26 @@
 const path = require("path");
 const express = require("express");
-const ipfilter = require("express-ipfilter").IpFilter;
-
-// Allow the following IPs
-const ips = ["62.162.180.176", "127.0.0.1"];
-
-// Create middleware.
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(ipfilter(ips, { mode: "allow" }));
+// Custom Middleware
+app.use((req, res, next) => {
+  let validIps = ["::1", "127.0.0.1", "62.162.180.176"];
+
+  console.log("remote >>", req.socket.remoteAddress);
+
+  if (validIps.includes(req.connection.remoteAddress)) {
+    // IP is ok, so go on
+    console.log("IP ok");
+    next();
+  } else {
+    // Invalid ip
+    console.log("Bad IP: " + req.connection.remoteAddress);
+    const err = new Error("Bad IP: " + req.connection.remoteAddress);
+    next(err);
+  }
+});
 
 const buildPath = path.join(__dirname, "..", "build");
 app.use(express.static(buildPath));
@@ -30,6 +40,12 @@ app.get("/api/jobs", async (req, res) => {
   } catch (error) {
     res.status(400).send("Error while getting list of jobs.Try again later.");
   }
+});
+
+app.use((err, req, res, next) => {
+  console.log("Error handler", err);
+  res.status(err.status || 500);
+  res.send("Something broke");
 });
 
 app.listen(PORT, () => {
