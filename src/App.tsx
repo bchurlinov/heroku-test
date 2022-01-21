a5a211ad455079fd81cb36fc48d6d014be403c12
bojan.churlinov <bchurlinov@hotmail.com>
2022-01-21 09:52:45 +0100

    Development changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
-import Button from 'components/button';
 import './App.css';
 
 const App = (): JSX.Element => (
   <div className="App">
     <header className="App-header">
-
-      <p>
-        Edit
-        <code>src/App.tsx</code>
-        and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-      <Button />
+      <h1>
+        This is Development
+      </h1>
     </header>
   </div>
 );
