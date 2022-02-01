f7a4fb5def4b953f6a59b1e2463b135f3276ee6d
Bojan <bchurlinov@hotmail.com>
2022-02-01 08:45:14 +0100

    heroku buildpack

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,9 @@ const App = (): JSX.Element => (
   <div className="App">
     <header className="App-header">
       <h1>
-        This is an App in Production Mode
+        This is an App in Production Mode with New Buildpack
       </h1>
+      <p>Test</p>
     </header>
   </div>
 );
