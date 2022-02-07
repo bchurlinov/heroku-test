bfe2477b1d0a8d97655c80b3bef710a7e64c28a3
Bojan <bchurlinov@hotmail.com>
2022-02-07 15:30:27 +0100

    node

--- server/server.js
+++ server/server.js
@@ -6,33 +6,33 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Custom Middleware
-// app.use((req, res, next) => {
-//   let validIps = ["62.162.180.176", "89.205.124.197"];
+app.use((req, res, next) => {
+  let validIps = ["62.162.180.176", "89.205.124.197"];
 
-//   var clientIp = requestIp.getClientIp(req);
-//   console.log("CLIENT IP >>", clientIp);
+  var clientIp = requestIp.getClientIp(req);
+  console.log("CLIENT IP >>", clientIp);
 
-//   if (validIps.includes(clientIp)) {
-//     // IP is ok, so go on
-//     next();
-//   } else {
-//     // Invalid ip
-//     const err = new Error("Bad IP: " + req.socket.remoteAddress);
-//     next(err);
-//   }
-// });
-
-app.use(requestIp.mw({ attributeName: "myCustomAttributeName" }));
-// respond to all requests
-app.use(function (req, res) {
-  // use our custom attributeName that we registered in the middleware
-  var ip = req.myCustomAttributeName;
-  console.log(ip);
-
-  // https://nodejs.org/api/net.html#net_net_isip_input
-  var ipType = net.isIP(ip); // returns 0 for invalid, 4 for IPv4, and 6 for IPv6
-  res.end("Hello, your ip address is " + ip + " and is of type IPv" + ipType + "\n");
+  if (validIps.includes(clientIp)) {
+    // IP is ok, so go on
+    next();
+  } else {
+    // Invalid ip
+    const err = new Error("Bad IP: " + req.socket.remoteAddress);
+    next(err);
+  }
 });
+
+// app.use(requestIp.mw({ attributeName: "myCustomAttributeName" }));
+// // respond to all requests
+// app.use(function (req, res) {
+//   // use our custom attributeName that we registered in the middleware
+//   var ip = req.myCustomAttributeName;
+//   console.log(ip);
+
+//   // https://nodejs.org/api/net.html#net_net_isip_input
+//   var ipType = net.isIP(ip); // returns 0 for invalid, 4 for IPv4, and 6 for IPv6
+//   res.end("Hello, your ip address is " + ip + " and is of type IPv" + ipType + "\n");
+// });
 
 const buildPath = path.join(__dirname, "..", "build");
 app.use(express.static(buildPath));
