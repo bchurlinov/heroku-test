0c2c8dd05360febb1f16dfdebdcc9a421b405500
Bojan <bchurlinov@hotmail.com>
2022-02-07 14:36:01 +0100

    react ip

--- server/server.js
+++ server/server.js
@@ -6,41 +6,36 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Custom Middleware
-app.use((req, res, next) => {
-  let validIps = ["62.162.180.176", "89.205.124.197"];
+// app.use((req, res, next) => {
+//   let validIps = ["62.162.180.176", "89.205.124.197"];
 
-  var clientIp = requestIp.getClientIp(req);
-  console.log("CLIENT IP >>", clientIp);
+//   var clientIp = requestIp.getClientIp(req);
+//   console.log("CLIENT IP >>", clientIp);
 
-  if (validIps.includes(clientIp)) {
-    // IP is ok, so go on
-    next();
-  } else {
-    // Invalid ip
-    const err = new Error("Bad IP: " + req.socket.remoteAddress);
-    next(err);
-  }
+//   if (validIps.includes(clientIp)) {
+//     // IP is ok, so go on
+//     next();
+//   } else {
+//     // Invalid ip
+//     const err = new Error("Bad IP: " + req.socket.remoteAddress);
+//     next(err);
+//   }
+// });
+
+app.use(requestIp.mw({ attributeName: "myCustomAttributeName" }));
+// respond to all requests
+app.use(function (req, res) {
+  // use our custom attributeName that we registered in the middleware
+  var ip = req.myCustomAttributeName;
+  console.log(ip);
+
+  // https://nodejs.org/api/net.html#net_net_isip_input
+  var ipType = net.isIP(ip); // returns 0 for invalid, 4 for IPv4, and 6 for IPv6
+  res.end("Hello, your ip address is " + ip + " and is of type IPv" + ipType + "\n");
 });
 
 const buildPath = path.join(__dirname, "..", "build");
 app.use(express.static(buildPath));
-
-app.get("/api/jobs", async (req, res) => {
-  try {
-    let { description = "", full_time, location = "", page = 1 } = req.query;
-
-    description = description ? encodeURIComponent(description) : "";
-    location = location ? encodeURIComponent(location) : "";
-    full_time = full_time === "true" ? "&full_time=true" : "";
-    if (page) {
-      page = parseInt(page);
-      page = isNaN(page) ? "" : `&page=${page}`;
-    }
-    res.send(["cool"]);
-  } catch (error) {
-    res.status(400).send("Error while getting list of jobs.Try again later.");
-  }
-});
 
 app.use((err, req, res, next) => {
   console.log("Error handler", err);
